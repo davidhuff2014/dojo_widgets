d137cad98e7f452bacc3698630c5fd404fc654c8
Dave Huff <David.Huff@computer-critters.com>
2015-09-05 19:38:48 -0400

    First commit

---
+++ lib/dojo_widgets/version.rb
@@ -0,0 +1,3 @@
+module DojoWidgets
+  VERSION = "0.1.0"
+end
---
+++ spec/dojo_widgets_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe DojoWidgets do
+  it 'has a version number' do
+    expect(DojoWidgets::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
